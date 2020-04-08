c0ad5f06ba7e78a9cfa2e92e15918b0a8690aecb
xiong35 <2242787668@qq.com>
2020-04-08 16:20:24 +0800

    start apps

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'artical',
+    'authorize',
+    'home',
+    'me',
+    'my_app',
+    'upload',
 ]
 
 MIDDLEWARE = [
@@ -103,18 +109,23 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, ""),
+    r'C:\Users\xiong35\Desktop\workingspace\blog\static',
+]
