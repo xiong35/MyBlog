039ea2cc37be6a48fe4172b68ea8ae52fc246c8d
xiong35 <2242787668@qq.com>
2020-04-18 10:54:38 +0800

    contributers half done

--- blog_vue/src/views/footer/components/Contributer.vue
+++ blog_vue/src/views/footer/components/Contributer.vue
@@ -2,35 +2,11 @@
   <div class="col-6 col-md-3" id="contributer">
     <h5 class="text-muted">Contributers</h5>
     <table class="table mt-3">
-      <tbody class="text-muted">
-        <tr>
-          <td>PM</td>
+      <tbody>
+        <tr v-for="(info, duty, index) in contributers" :key="index">
+          <td class="text-muted">{{duty}}</td>
           <td>
-            <a href="http://www.xiong35.com/static/home/index.html" class="text-muted">xiong35</a>
-          </td>
-        </tr>
-        <tr>
-          <td>UI</td>
-          <td>
-            <a href="http://www.xiong35.com/static/home/index.html" class="text-muted">xiong35</a>
-          </td>
-        </tr>
-        <tr>
-          <td>前端</td>
-          <td>
-            <a href="http://www.xiong35.com/static/home/index.html" class="text-muted">xiong35</a>
-          </td>
-        </tr>
-        <tr>
-          <td>后端</td>
-          <td>
-            <a href="http://www.xiong35.com/static/home/index.html" class="text-muted">xiong35</a>
-          </td>
-        </tr>
-        <tr>
-          <td>运维</td>
-          <td>
-            <a href="http://www.xiong35.com/static/home/index.html" class="text-muted">xiong35</a>
+            <a :href="info.link" class="text-muted">{{info.name}}</a>
           </td>
         </tr>
       </tbody>
@@ -39,11 +15,23 @@
 </template>
 
 <script>
+  let xiong35 = {
+    name: "xiong35",
+    link: "http://www.xiong35.com/static/home/index.html"
+  };
   export default {
     name: "Contributer",
     components: {},
     data() {
-      return {};
+      return {
+        contributers: {
+          PM: xiong35,
+          UI: xiong35,
+          前端: xiong35,
+          后端: xiong35,
+          运维: xiong35
+        }
+      };
     },
     computed: {},
     watch: {},
