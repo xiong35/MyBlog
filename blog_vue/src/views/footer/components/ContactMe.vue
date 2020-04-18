3c173fd068958f7e514016c1f28d33f2f2e699e1
xiong35 <2242787668@qq.com>
2020-04-18 11:46:21 +0800

    add img to contact me

--- blog_vue/src/views/footer/components/ContactMe.vue
+++ blog_vue/src/views/footer/components/ContactMe.vue
@@ -2,7 +2,14 @@
   <div class="col-6 col-md-3 icon" id="contact-me">
     <h5 class="pb-2 text-secondary">Contact Me!</h5>
     <ul class="list-unstyled text-justify">
-      <li>
+      <li v-for="(item, index) in linkList" :key="index">
+        <a v-if="item.link" :href="item.link">
+          <i :class="item.iclass" class="fa"></i>
+        </a>
+        <i v-else :class="item.iclass" class="fa"></i>
+        <img v-if="item.img" v-show="item.show" :src="item.img" alt="二维码" />
+      </li>
+      <!-- <li>
         <a href="https://github.com/xiong35">
           <i
             class="fa text-secondary fa-github m-2"
@@ -12,12 +19,12 @@
       </li>
       <li>
         <i class="fa text-secondary fa-qq m-2" style="font-size:1.6rem"></i>
-        <img style="display: none;" src="q.jpg" alt="qq二维码" class="img-thumbnail bin_code" />
+        <img src="q.jpg" alt="qq二维码" class="img-thumbnail bin_code" />
       </li>
       <li>
         <i class="fa text-secondary fa-weixin m-2" style="font-size:1.6rem"></i>
         <img style="display: none;" src="v.jpg" alt="微信二维码" class="img-thumbnail bin_code" />
-      </li>
+      </li>-->
     </ul>
   </div>
 </template>
@@ -27,7 +34,28 @@
     name: "ContactMe",
     components: {},
     data() {
-      return {};
+      return {
+        linkList: [
+          {
+            iclass: "fa-github",
+            link: "https://github.com/xiong35",
+            img: null,
+            show: false
+          },
+          {
+            iclass: "fa-qq",
+            link: null,
+            img: require("assets/img/q.jpg"),
+            show: false
+          },
+          {
+            iclass: "fa-weixin",
+            link: null,
+            img: require("assets/img/v.jpg"),
+            show: false
+          }
+        ]
+      };
     },
     computed: {},
     watch: {},
