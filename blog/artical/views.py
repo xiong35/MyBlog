6b7d288eefd314a379df3361e02cecbd5bdea602
xiong35 <2242787668@qq.com>
2020-05-05 22:11:51 +0800

    artical GET api

--- blog/artical/views.py
+++ blog/artical/views.py
@@ -1,12 +1,28 @@
+
 from django.http import JsonResponse
 from django.views.generic import View
-# Create your views here.
+
+from .models import Blog as BlogModel
+from .models import ArticalTag
+from .models import Trap as TrapModel
 
 
 class Blog(View):
 
     def get(self, request):
-        return JsonResponse({1: "12"})
+
+        get_id = request.GET.get('id', None)
+        if get_id:
+            qs = BlogModel.objects.filter(pk=get_id).values('content')
+            return JsonResponse({'state': 200, 'content': list(qs)[0]})
+
+        json_data = [
+            {'id': b.id,
+             'tags': [a.tag_name for a in b.tags.all()],
+             'last_update':b.last_update,
+             'headline':b.headline} for b in BlogModel.objects.prefetch_related('tags')
+        ]
+        return JsonResponse({'state': 200, 'data': json_data})
 
     def post(self, request):
         return JsonResponse({})
