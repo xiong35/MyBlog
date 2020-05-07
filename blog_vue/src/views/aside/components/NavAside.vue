82bbda826fdcaa6faf3236582017cd865a9c03bc
xiong35 <2242787668@qq.com>
2020-05-07 20:44:46 +0800

    aside glimmer

--- blog_vue/src/views/aside/components/NavAside.vue
+++ blog_vue/src/views/aside/components/NavAside.vue
@@ -1,60 +1,23 @@
 // dependency: 
 // 
 <template>
-  <ul class="list-unstyled nav-aside pl-4 pt-2">
-    <li v-for="(item, index) in links" :key="index">
-      <h4 class="nav-class" href="item.href">{{item.name}}</h4>
-      <ul>
-        <li
-          v-for="(navItem, index) in item.items"
-          v-show="navItem.show"
-          :href="navItem.href"
-          :key="index"
-        >{{navItem.name}}</li>
-      </ul>
+  <ul class="list-unstyled">
+    <li class="m-3 nav-aside-li" v-for="(item, index) in links" :key="index">
+      <div class="nav-aside-div">{{item.name}}</div>
+      <div class="x-v"></div>
+      <div class="x-h"></div>
+      <div class="y-v"></div>
+      <div class="y-h"></div>
     </li>
   </ul>
 </template>
 
 <script>
   let links = [
-    {
-      name: "xxx",
-      href: "#",
-      items: [
-        { name: "iiiii", href: "#", show: true },
-        { name: "iiiii", href: "#", show: false },
-        { name: "iiiii", href: "#", show: true }
-      ]
-    },
-    {
-      name: "xxx",
-      href: "#",
-      items: [
-        { name: "iiiii", href: "#", show: true },
-        { name: "iiiii", href: "#", show: true },
-        { name: "iiiii", href: "#", show: true },
-        { name: "iiiii", href: "#", show: true }
-      ]
-    },
-    {
-      name: "xxx",
-      href: "#",
-      items: [
-        { name: "iiiii", href: "#", show: true },
-        { name: "iiiii", href: "#", show: true },
-        { name: "iiiii", href: "#", show: true }
-      ]
-    },
-    {
-      name: "xxx",
-      href: "#",
-      items: [
-        { name: "iiiii", href: "#", show: true },
-        { name: "iiiii", href: "#", show: true },
-        { name: "iiiii", href: "#", show: true }
-      ]
-    }
+    { name: "文章", link: "/artical" },
+    { name: "文章", link: "/artical" },
+    { name: "文章", link: "/artical" },
+    { name: "文章", link: "/artical" }
   ];
   export default {
     name: "NavAside",
@@ -72,7 +35,71 @@
   };
 </script>
 <style scoped>
-  .nav-item:before {
-    content: " - ";
+  ul {
+    text-align: center;
+  }
+  li {
+    display: inline-block;
+    width: 80%;
+    margin: auto;
+    line-height: 3rem;
+    position: relative;
+    cursor: pointer;
+  }
+  .x-v,
+  .y-v,
+  .x-h,
+  .y-h {
+    position: absolute;
+    border-width: 1px;
+    border-color: #000;
+    transition: all 0.5s ease;
+  }
+  .x-h,
+  .x-v {
+    width: 107%;
+    height: 100%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .y-h,
+  .y-v {
+    width: 100%;
+    height: 130%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .x-v,
+  .y-v {
+    border-style: none solid;
+  }
+  .x-h,
+  .y-h {
+    border-style: solid none;
+  }
+  .nav-aside-li:hover .x-h {
+    width: 100%;
+    border-color: #007bff;
+  }
+  .nav-aside-li:hover .y-v {
+    height: 100%;
+    border-color: #007bff;
+  }
+  .nav-aside-li:hover .x-v {
+    height: 0;
+    border-color: #007bff;
+  }
+  .nav-aside-li:hover .y-h {
+    width: 0;
+    border-color: #007bff;
+  }
+  .nav-aside-li:hover .nav-aside-div {
+    color: #007bff;
+    box-shadow: 0 0 10px #209bff86;
+  }
+  .nav-aside-div {
+    transition: all 0.7s ease;
   }
 </style>
