8b49b65586166ee8bf8dab01b82c67e3e8a482be
xiong35 <2242787668@qq.com>
2020-05-11 22:30:50 +0800

    mysql

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': 'xyl0321',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
